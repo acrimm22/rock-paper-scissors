c1023db97ee5c48c281011b09166fb856b86a9a0
acrimm22 <atcrimmins22@gmail.com>
2024-04-18 14:04:04 -0500

    Completed the algorithm/psuedocode

---
+++ myscript.js
@@ -0,0 +1,43 @@
+//Return a value for the computer's choice
+
+//Prompt the player to enter a value
+
+//Compare the computer's choice with the player's choice
+
+  //If the computer picks rock and the player picks paper
+  //the player wins; increase player count by 1 and game count by 1
+  //return "Paper beats Rock, you WIN!"
+
+  //If the computer picks rock and the player picks scissors
+  //the player loses; increase computer count by 1 and game count by 1
+  //return "Rock beats Scissors, you LOSE!"
+
+  //If the computer picks paper and the player picks rock
+  //the player loses; increase computer count by 1 and game count by 1
+  //return "Paper beats Rock, you LOSE!"
+
+  //If the computer picks paper and the player picks scissors
+  //the player wins; increase player count by 1 and game count by 1
+  //return "Scissors beats Paper, you WIN!"
+
+  //If the computer picks scissors and the player picks rock
+  //the player wins; increase player count by 1 and game count by 1
+  //return "Rock beats Scissors, you WIN!"
+
+  //If the computer picks scissors and the player picks paper
+  //the player loses; increase computer count by 1 and game count by 1
+  //return "Scissors beats Paper, you LOSE!"
+
+  //Else, if the two values are equal, it is a tie.
+  //Retry the turn and game count by 1
+
+//When the game count is 5, compare the win count
+//of the computer to the player
+  //If the player's count > computer, return "You WIN the GAME!"
+  
+  //If the player's count < computer, return "You LOSE the GAME! TRY AGAIN"
+
+  //If the player's count = computer, return "It's a DRAW GAME! PLAY AGAIN"
+  
+
+  
