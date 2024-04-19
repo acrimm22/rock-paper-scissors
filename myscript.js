7dba11a598e7aed41f8f03e4d85d9e9f64e2b17c
acrimm22 <atcrimmins22@gmail.com>
2024-04-19 11:32:29 -0500

    Added round and score counter functions and variables

--- myscript.js
+++ myscript.js
@@ -1,9 +1,4 @@
-//Return a value for the computer's choice variable:
-//Ask the computer to return either a 0, 1, or 2
-  //If the value = 0 the computer's choice is Paper
-  //If the value = 1 the computer's choice is Rock
-  //If the value = 2 the computer's choice is Scissors
-
+//Function for the computer to choose rock, paper, or scissors
 function getComputerChoice(max) {
   number = Math.floor(Math.random() * max);
   if (number === 0) {
@@ -15,80 +10,50 @@ function getComputerChoice(max) {
   }
 }
 
-const computerSelection = getComputerChoice(3);
+//Round and Score Counters
+function increaseRoundCount(a) {
+  return roundCount = a + 1;
+}
+function increaseComputerScore(a) {
+  return computerScore = a + 1;
+}
+function increasePlayerScore(a) {
+  return playerScoreScore = a + 1;
+}
 
-//Prompt the player to enter a value and return that into
-//the player's choice variable
+
+function playRound(computerSelection, playerSelectionCaseSensitive) {
+  if (computerSelection === "rock" && playerSelectionCaseSensitive === "paper") {
+    alert ("Paper beats Rock, you WIN!");
+    } else if (computerSelection === "rock" && playerSelectionCaseSensitive === "scissors") {
+      alert ("Rock beats Scissors, you LOSE!");
+    } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "rock") {
+      alert ("Paper beats Rock, you LOSE!");
+    } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "scissors") {
+      alert ("Scissors beats Paper, you WIN!");
+    } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "rock") {
+      alert ("Rock beats Scissors, you WIN!");
+    } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "paper") {
+      alert ("Scissors beats Paper, you LOSE!");
+    } else {
+      alert ("TIE");
+  }
+}
+
+//variables
+const computerSelection = getComputerChoice(3);
 
 const playerSelection = prompt("Type: Rock, Paper, or Scissors");
 const playerSelectionCaseSensitive = playerSelection.toLowerCase(playerSelection.trim());
 
-//Compare the computer's choice with the player's choice
-//Start the round counter for 5 rounds
+let roundCount = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-  //If the computer picks rock and the player picks paper
-  //the player wins; increase player count by 1 and game count by 1
-  //return "Paper beats Rock, you WIN!"
 
-if (computerSelection === "rock" && playerSelectionCaseSensitive === "paper") {
-  alert ("Paper beats Rock, you WIN!");
-}
-  //If the computer picks rock and the player picks scissors
-  //the player loses; increase computer count by 1 and game count by 1
-  //return "Rock beats Scissors, you LOSE!"
 
-  else if (computerSelection === "rock" && playerSelectionCaseSensitive === "scissors") {
-    alert ("Rock beats Scissors, you LOSE!");
-  }
 
-  //If the computer picks paper and the player picks rock
-  //the player loses; increase computer count by 1 and game count by 1
-  //return "Paper beats Rock, you LOSE!"
 
-  else if (computerSelection === "paper" && playerSelectionCaseSensitive === "rock") {
-    alert ("Paper beats Rock, you LOSE!");
-  }
-
-  //If the computer picks paper and the player picks scissors
-  //the player wins; increase player count by 1 and game count by 1
-  //return "Scissors beats Paper, you WIN!"
-
-  else if (computerSelection === "paper" && playerSelectionCaseSensitive === "scissors") {
-    alert ("Scissors beats Paper, you WIN!");
-  }
-
-  //If the computer picks scissors and the player picks rock
-  //the player wins; increase player count by 1 and game count by 1
-  //return "Rock beats Scissors, you WIN!"
-
-  else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "rock") {
-    alert ("Rock beats Scissors, you WIN!");
-  }
-
-  //If the computer picks scissors and the player picks paper
-  //the player loses; increase computer count by 1 and game count by 1
-  //return "Scissors beats Paper, you LOSE!"
-
-  else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "paper") {
-    alert ("Scissors beats Paper, you LOSE!");
-  }
-
-  //Else, if the two values are equal, it is a tie.
-  //Retry the turn and game count by 1
-
-  else {
-    alert ("TIE");
-  }
-
-//When the game count is 5, compare the win count
-//of the computer to the player
-  //If the player's count > computer, return "You WIN the GAME!"
-  
-  //If the player's count < computer, return "You LOSE the GAME! TRY AGAIN"
-
-  //If the player's count = computer, return "It's a DRAW GAME! PLAY AGAIN"
-
-//Variables:
 
 
   
---
+++ pseudocode.js
@@ -0,0 +1,49 @@
+//Return a value for the computer's choice variable:
+//Ask the computer to return either a 0, 1, or 2
+  //If the value = 0 the computer's choice is Paper
+  //If the value = 1 the computer's choice is Rock
+  //If the value = 2 the computer's choice is Scissors
+
+  //Prompt the player to enter a value and return that into
+//the player's choice variable
+
+
+//Compare the computer's choice with the player's choice
+//Start the round counter for 5 rounds
+
+  //If the computer picks rock and the player picks paper
+  //the player wins; increase player count by 1 and game count by 1
+  //return "Paper beats Rock, you WIN!"
+
+    //If the computer picks rock and the player picks scissors
+  //the player loses; increase computer count by 1 and game count by 1
+  //return "Rock beats Scissors, you LOSE!"
+
+    //If the computer picks paper and the player picks rock
+  //the player loses; increase computer count by 1 and game count by 1
+  //return "Paper beats Rock, you LOSE!"
+
+    //If the computer picks paper and the player picks scissors
+  //the player wins; increase player count by 1 and game count by 1
+  //return "Scissors beats Paper, you WIN!"
+
+    //If the computer picks scissors and the player picks rock
+  //the player wins; increase player count by 1 and game count by 1
+  //return "Rock beats Scissors, you WIN!"
+
+  //If the computer picks scissors and the player picks paper
+  //the player loses; increase computer count by 1 and game count by 1
+  //return "Scissors beats Paper, you LOSE!"
+
+    //Else, if the two values are equal, it is a tie.
+  //Retry the turn and game count by 1
+
+  //When the game count is 5, compare the win count
+//of the computer to the player
+  //If the player's count > computer, return "You WIN the GAME!"
+  
+  //If the player's count < computer, return "You LOSE the GAME! TRY AGAIN"
+
+  //If the player's count = computer, return "It's a DRAW GAME! PLAY AGAIN"
+
+//Variables:
