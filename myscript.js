3ed29a9da2950c1a07c8b7cc8a1914f9511b9ff1
acrimm22 <atcrimmins22@gmail.com>
2024-04-19 15:56:28 -0500

    Reverted to previous rendition via github due to complexity

--- myscript.js
+++ myscript.js
@@ -10,35 +10,31 @@ function getComputerChoice(max) {
   }
 }
 
-function playGame() {
-  let roundCount = 0;
-  let playerScore = 0;
-  let computerScore = 0;
-
-  function playRound(playerSelectionCaseSensitive, computerSelection) {
-    if (computerSelection === "rock" && playerSelectionCaseSensitive === "paper") {
-      return "Paper beats Rock, you WIN!";
+function playRound(playerSelectionCaseSensitive, computerSelection) {
+  if (computerSelection === "rock" && playerSelectionCaseSensitive === "paper") {
+    return "Paper beats Rock, you WIN!";
+    
+    } else if (computerSelection === "rock" && playerSelectionCaseSensitive === "scissors") {
+      return "Rock beats Scissors, you LOSE!";
       
-      } else if (computerSelection === "rock" && playerSelectionCaseSensitive === "scissors") {
-        return "Rock beats Scissors, you LOSE!";
-        
-      } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "rock") {
-        return "Paper beats Rock, you LOSE!";
-        
-      } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "scissors") {
-        return "Paper beats Scissors, you WIN!";
-        
-      } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "rock") {
-        return "Rock beats Scissors, you WIN!";
-        
-      } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "paper") {
-        return "Scissors beats Paper, you LOSE!";
-        
-      } else {
-        return "TIE";
-    }
+    } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "rock") {
+      return "Paper beats Rock, you LOSE!";
+      
+    } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "scissors") {
+      return "Paper beats Scissors, you WIN!";
+      
+    } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "rock") {
+      return "Rock beats Scissors, you WIN!";
+      
+    } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "paper") {
+      return "Scissors beats Paper, you LOSE!";
+      
+    } else {
+      return "TIE";
   }
+
 }
+
 //variables
 const computerSelection = getComputerChoice(3);
 
