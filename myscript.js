6022f20a41b7b6972ce924a911299e371870092c
acrimm22 <atcrimmins22@gmail.com>
2024-04-21 20:53:46 -0500

    Completed the single round code that also scores the rounds

--- myscript.js
+++ myscript.js
@@ -1,5 +1,6 @@
 //Function for the computer to choose rock, paper, or scissors
 function getComputerChoice(max) {
+  max = 3;
   number = Math.floor(Math.random() * max);
   if (number === 0) {
     return "paper";
@@ -10,38 +11,52 @@ function getComputerChoice(max) {
   }
 }
 
-function playRound(playerSelectionCaseSensitive, computerSelection) {
-  if (computerSelection === "rock" && playerSelectionCaseSensitive === "paper") {
-    return "Paper beats Rock, you WIN!";
-    
-    } else if (computerSelection === "rock" && playerSelectionCaseSensitive === "scissors") {
-      return "Rock beats Scissors, you LOSE!";
-      
-    } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "rock") {
-      return "Paper beats Rock, you LOSE!";
-      
-    } else if (computerSelection === "paper" && playerSelectionCaseSensitive === "scissors") {
-      return "Paper beats Scissors, you WIN!";
-      
-    } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "rock") {
-      return "Rock beats Scissors, you WIN!";
-      
-    } else if (computerSelection === "scissors" && playerSelectionCaseSensitive === "paper") {
-      return "Scissors beats Paper, you LOSE!";
-      
-    } else {
-      return "TIE";
-  }
-
+//Function for the user to imput rock, paper, or scissors
+function getHumanChoice(){
+  let choice = prompt("Type: Rock, Paper, or Scissors").toLowerCase();
+  return choice;
 }
 
-//variables
-const computerSelection = getComputerChoice(3);
+//Function to play one round of rock, paper, scissors; increases the score and counts the game;
+function playRound(playerChoice, computerChoice) {
+    
+  if (computerChoice == "rock" && playerChoice == "paper") {
+    console.log("Paper beats Rock, you WIN!");
+    return humanScore++;
+    
+    } else if (computerChoice == "rock" && playerChoice == "scissors") {
+      console.log("Rock beats Scissors, you LOSE!");
+      return computerScore++;
+      
+    } else if (computerChoice == "paper" && playerChoice == "rock") {
+      console.log("Paper beats Rock, you LOSE!");
+      return computerScore++;
+      
+    } else if (computerChoice == "paper" && playerChoice == "scissors") {
+      console.log("Scissors beats Paper, you WIN!");
+      return humanScore++;
+      
+    } else if (computerChoice == "scissors" && playerChoice == "rock") {
+      console.log("Rock beats Scissors, you WIN!");
+      return humanScore++;
+      
+    } else if (computerChoice == "scissors" && playerChoice == "paper") {
+      console.log("Scissors beats Paper, you LOSE!");
+      return computerScore++;
+      
+    } else {
+        console.log("TIE");
+        return;
+  }
+}
 
-const playerSelection = prompt("Type: Rock, Paper, or Scissors");
-const playerSelectionCaseSensitive = playerSelection.toLowerCase(playerSelection.trim());
+//Variables for Scoring
+let humanScore = 0;
+let computerScore = 0;
 
-console.log(playRound(playerSelectionCaseSensitive, computerSelection));
 
+//variables for choosing values to compare
+let computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
 
-  
+playRound(humanSelection, computerSelection);
